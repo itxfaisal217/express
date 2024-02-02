65cbd7db5d3cbd640fd5da0ad986f74f571e3ece
brandonheacock <heacockbrandon743@gmail.com>
2024-02-03 03:42:17 +0500

    new changes

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
 
 // Import Mongoose model(s)
 const UserModel = require('./models/userSchema');
 const ProductModel = require('./models/productSchema');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // MongoDB Atlas connection URL
-const mongoURI =
-  'mongodb+srv://faisalnazirpv11217:3YMtftZIT81OwxqQ@cluster0.locsgbv.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = process.env.MONGODB_URI;
 
 // Connect to MongoDB Atlas
 mongoose
@@ -21,12 +19,8 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((data) => {
+  .then(() => {
     console.log('Connected to MongoDB Atlas');
-    // Start the server after successfully connecting to the database
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB Atlas:', error);
@@ -36,7 +30,7 @@ mongoose
 app.get('/api/users', async (req, res) => {
   try {
     const users = await UserModel.find();
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -57,39 +51,39 @@ app.post('/api/users', async (req, res) => {
 
 // Route to get a user by ID
 app.get('/api/users/:userId', async (req, res) => {
-    try {
-      const { userId } = req.params;
-      const user = await UserModel.findById(userId);
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      res.json(user);
-    } catch (error) {
-      console.error('Error fetching User by ID:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { userId } = req.params;
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
+    res.json(user);
+  } catch (error) {
+    console.error('Error fetching User by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-  // Route to delete a user by ID
+// Route to delete a user by ID
 app.delete('/api/users/:userId', async (req, res) => {
-    try {
-      const { userId } = req.params;
-      const deletedUser = await UserModel.findByIdAndDelete(userId);
-      if (!deletedUser) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      res.status(200).json({ message: 'User deleted successfully', deletedUser });
-    } catch (error) {
-      console.error('Error deleting User by ID:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { userId } = req.params;
+    const deletedUser = await UserModel.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  });
+    res.status(200).json({ message: 'User deleted successfully', deletedUser });
+  } catch (error) {
+    console.error('Error deleting User by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // Example route for fetching products
 app.get('/api/products', async (req, res) => {
   try {
     const products = await ProductModel.find();
-    res.json(products);
+    res.status(200).json(products);
   } catch (error) {
     console.error('Error fetching products:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -110,30 +104,32 @@ app.post('/api/products', async (req, res) => {
 
 // Route to get a product by ID
 app.get('/api/products/:productId', async (req, res) => {
-    try {
-      const { productId } = req.params;
-      const product = await ProductModel.findById(productId);
-      if (!product) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
-      res.json(product);
-    } catch (error) {
-      console.error('Error fetching product by ID:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { productId } = req.params;
+    const product = await ProductModel.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
     }
-  });
+    res.json(product);
+  } catch (error) {
+    console.error('Error fetching product by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-  // Route to delete a product by ID
+// Route to delete a product by ID
 app.delete('/api/products/:productId', async (req, res) => {
-    try {
-      const { productId } = req.params;
-      const deletedProduct = await ProductModel.findByIdAndDelete(productId);
-      if (!deletedProduct) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
-      res.status(200).json({ message: 'Product deleted successfully', deletedProduct });
-    } catch (error) {
-      console.error('Error deleting product by ID:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { productId } = req.params;
+    const deletedProduct = await ProductModel.findByIdAndDelete(productId);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
     }
-  });
+    res.status(200).json({ message: 'Product deleted successfully', deletedProduct });
+  } catch (error) {
+    console.error('Error deleting product by ID:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = app;
